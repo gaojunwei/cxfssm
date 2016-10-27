ab769edb7453387886268639c45e45c60a9b2102
高军威 <gjw.5@qq.com>
2016-10-27 17:47:31 +0800

    csf quartz

--- src/com/wlsq/oauth/quartz/SpringQtz.java
+++ src/com/wlsq/oauth/quartz/SpringQtz.java
@@ -1,13 +1,11 @@
 package com.wlsq.oauth.quartz;
 
-import java.util.Date;
 
 public class SpringQtz {
 	private static int counter = 0;
 
 	protected void execute() {
 		long ms = System.currentTimeMillis();
-		System.out.println("\t\t" + new Date(ms));
-		System.out.println("(" + counter++ + ")");
+		System.out.println("quartz 定时任务系统--执行次数："+(++counter));
 	}
 }
